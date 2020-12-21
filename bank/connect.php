6330923fca89157961e12ce78e9c415e61a6f245
rishikakikkuri <43698970+rishikakikkuri@users.noreply.github.com>
2020-12-21 07:15:57 +0530

    bank

---
+++ bank/connect.php
@@ -0,0 +1,14 @@
+<?php
+$host="sql301.epizy.com";
+$user = "epiz_27149607";
+$password="RPErGWYzqjscEF";
+$db="epiz_27149607_BBS";
+
+
+$success=mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $db
+);
+?>
